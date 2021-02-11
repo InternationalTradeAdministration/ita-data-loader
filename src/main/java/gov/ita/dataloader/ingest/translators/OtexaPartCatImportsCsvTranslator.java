b80903892442da3da0dc4acc2a5d2ee9de283d6f
Donna Hogan <ddhogan@users.noreply.github.com>
2021-02-11 17:51:23 -0500

    [19342] Otexa: Part Category data adds unit columns (#96)

--- src/main/java/gov/ita/dataloader/ingest/translators/OtexaPartCatImportsCsvTranslator.java
+++ src/main/java/gov/ita/dataloader/ingest/translators/OtexaPartCatImportsCsvTranslator.java
@@ -24,7 +24,7 @@ public class OtexaPartCatImportsCsvTranslator implements Translator {
 
     try {
       csvPrinter = new CSVPrinter(stringWriter, CSVFormat.DEFAULT
-        .withHeader("country", "PART CAT", "CAT_ID", "HTS", "SYEF", "YR", "MON","HEADER_ID", "VAL"));
+        .withHeader("country", "PART CAT", "CAT_ID", "HTS", "SYEF", "UOM", "M2", "DOLLAR_SIGN", "YR", "MON", "HEADER_ID", "VAL"));
 
       Reader reader = new CharSequenceReader(new String(bytes));
       CSVParser csvParser;
@@ -44,6 +44,9 @@ public class OtexaPartCatImportsCsvTranslator implements Translator {
         String catId = csvRecord.get("CAT");
         String hts = csvRecord.get("HTS");
         String syef = csvRecord.get("SYEF");
+        String uom = csvRecord.get("UOM");
+        String m2 = csvRecord.get("M2");
+        String dollarSign = csvRecord.get("$");
         String yr = csvRecord.get("YR");
         String mon = csvRecord.get("MON");
 
@@ -52,7 +55,7 @@ public class OtexaPartCatImportsCsvTranslator implements Translator {
           if (val != null) {
             if (snt.isScientificNotation(val)) val = snt.translate(val);
             csvPrinter.printRecord(
-              country, partCat, catId, hts, syef, yr, mon, header, val
+              country, partCat, catId, hts, syef, uom, m2, dollarSign, yr, mon, header, val
             );
           }
         }
